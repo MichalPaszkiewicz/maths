4b0a742e3ba2fc4b4f08f879a1b2e457b51605bb
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-10-03 13:18:33 +0100

    Update page-specific.js

--- bignumber/page-specific.js
+++ bignumber/page-specific.js
@@ -1,1 +1,19 @@
+//only works for positive for now!
+var getNumberOne = function(){
+	return new number( true, $("#number-one").text() );
+}
+
+var getNumberTwo = function(){
+	return new number( true, $("#number-two").text() );
+}
+
  
+var doAdd = function(){
+	var result = getNumberOne().plus(getNumberTwo());
+ 	$("#result").text() = result;
+}
+
+var doSubtract = function(){
+	var result = getNumberOne().minus(getNumberTwo());
+ 	$("#result").text() = result;
+}
