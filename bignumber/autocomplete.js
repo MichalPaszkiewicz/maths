19d4770aa7655a1c049ad35ad86dc92b4d75ded4
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-10-03 17:40:46 +0100

    Update autocomplete.js

--- bignumber/autocomplete.js
+++ bignumber/autocomplete.js
@@ -38,7 +38,7 @@ function fullOptionString(items)
 	for(var i = 0; i < items.length; i++)
 	{
 		if(i == 0){ result += optionString(items[i], i, "selected") }
-		else{	result += optionString(items[i]); }
+		else{	result += optionString(items[i], i); }
 	}
 	return result;
 }
