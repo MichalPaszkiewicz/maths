abd5193a2759357446ad2982dac93381a9f47839
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2014-10-06 10:05:05 +0100

    Update number.js

--- bignumber/number.js
+++ bignumber/number.js
@@ -255,31 +255,3 @@ number.prototype.getHex = function()
 	
 	return hexString;
 }
-
-function complexNumber( realPositive, realValue, iPositive, iValue)
-{
-	this.real = new number( realPositive, realValue );
-	this.i = new number( iPositive, iValue );
-	this.toString = function()
-	{
-		var realSign = this.real.positive ? "+" : "-";
-		var iSign = this.i.positive ? "+" : "-";
-		return realSign + this.real.value + iSign + this.i.value + "i";
-	}
-}
-
-complexNumber.prototype.plus = function(otherComplexNum)
-{
-	var newReal = this.real.plus(otherComplexNum.real);
-	var newI = this.i.plus(otherComplexNum.i);
-	
-	return new complexNumber( newReal.positive, newReal.value, newI.positive, newI.value );
-}
-
-complexNumber.prototype.minus = function(otherComplexNum)
-{
-	var newReal = this.real.minus(otherComplexNum.real);
-	var newI = this.i.minus(otherComplexNum.i);
-	
-	return new complexNumber( newReal.positive, newReal.value, newI.positive, newI.value );
-}
