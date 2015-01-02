5d3bce6cc2d7943bae07507ee7c6599789650a4d
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2015-01-02 12:02:40 +0000

    Update gameTheoryController.js

--- game-theory/gameTheoryController.js
+++ game-theory/gameTheoryController.js
@@ -1,5 +1,23 @@
 var gameModule = angular.module('app', []).
 	controller("gameTheoryControl", function gameTheoryControl($scope){
-	    $scope.numbers=[{name: 1},{name: 2}, {name: 3}];
+	    	
+	    	function number(num){
+	    		this.name = num; 
+	    		return this;
+	    	}
+	    	
+	    	$scope.numbers=[{name: 1},{name: 2}, {name: 3}];
+	    	
+	    	$scope.amount = 10;
+	    	
+	    	$scope.newNumbers = function(){
+			$scope.numbers = [];
+			
+			intAmount = parseInt($scope.amount);
+			
+			for(var i = 0; i < intAmount; i++){
+				$scope.numbers.push(new number(i));
+			}
+		}
 	
 	});
